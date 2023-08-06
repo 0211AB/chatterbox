997ddf0b1b3955880bd5a65a564676f9fbe1b4f3
0211AB <abhaybajajit@gmail.com>
2023-08-07 02:31:18 +0530

    Updated server for cors

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,8 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://main--chatter-box-frontend.netlify.app"
     // credentials: true,
   },
 });
